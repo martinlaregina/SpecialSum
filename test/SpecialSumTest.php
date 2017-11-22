3ce0c83af0b3dbae4830e36174b8af66b033e3a7
Martin A. La Regina <laregina@gmail.com>
2017-11-22 18:36:05 +0100

    Add test directory
    To run the test use PHPUnit
    
    Happy Coding

---
+++ test/SpecialSumTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use app\libraries\SpecialSum;
+
+class SpecialSumTest extends PHPUnit_Framework_TestCase
+{
+    protected $SpecialSum;
+
+    public function inputNumbers()
+    {
+        return [
+            [1, 3, 6],
+            [2, 3, 10],
+            [4, 10, 2002],
+            [10, 10, 167960],
+            [20, 20, 131282408400],
+            [30, 30, 114449595062769120],
+            [100, 100, 0],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider inputNumbers
+     */
+    public function testCalculate($x, $y, $result)
+    {
+        $this->SpecialSum = SpecialSum::start();
+        if ($x==100 and $y==100){
+            print('And the winner is '.$this->SpecialSum->calculate($x, $y));
+        } else {
+            $this->assertEquals($result, $this->SpecialSum->calculate($x, $y));
+        }
+
+    }
+
+}
